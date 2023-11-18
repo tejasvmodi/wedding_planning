9cbb899eec14d72e84e4a5ec627e5a571fcc13d2
tejasvmodi <moditejasv@gmail.com>
2023-11-18 16:39:08 +0530

    commit at 16:38  18 -11-2023

--- lib/screens/components/BottomNavigation.dart
+++ lib/screens/components/BottomNavigation.dart
@@ -1,34 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:wedding_application/screens/Authentication/forms/forgotpassword.dart';
+import 'package:wedding_application/screens/Authentication/forms/loginform.dart';
+import 'package:wedding_application/screens/components/OnboardingScreen.dart';
 
-class BottomNavigationBar123 extends StatelessWidget {
+class BottomNavigationBar123 extends StatefulWidget {
   const BottomNavigationBar123({super.key});
 
   @override
+  State<BottomNavigationBar123> createState() => _BottomNavigationBar123State();
+}
+
+class _BottomNavigationBar123State extends State<BottomNavigationBar123> {
+  int _currentIndex = 0;
+
+  // ignore: non_constant_identifier_names
+  final List<Widget> _Items = [
+    const LoginF(),
+    const ForgotPassword(),
+    const OnboardingScreen(),
+    const LoginF(),
+    const OnboardingScreen()
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        height: 50,
-        decoration: const BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-        child: Row(
-          children: [
-            Column(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.home),
-                ),
-                const Text("Home")
-              ],
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.home_filled),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text("Wedding Application"),
+      ),
+      body: _Items[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              backgroundColor: Colors.black,
+              label: "Home"),
+
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              backgroundColor: Colors.black,
+              label: "Home"),
+
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              backgroundColor: Colors.black,
+              label: "Home"),
+
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              backgroundColor: Colors.black,
+              label: "Home"),
+
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              backgroundColor: Colors.black,
+              label: "Home"),
+        ],
       ),
     );
   }
