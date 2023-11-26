781909d171a7ff8e942d2d939c0f2c24f1e39685
tejasvmodi <moditejasv@gmail.com>
2023-11-26 17:50:42 +0530

    commit at 17:50  26-11-2023

--- lib/screens/Home/checklist.dart
+++ lib/screens/Home/checklist.dart
@@ -9,6 +9,22 @@ class CheckList extends StatefulWidget {
 }
 
 class _CheckListState extends State<CheckList> {
+  final List<String> choice = [
+    "item 1",
+    "item 2",
+    "item 3",
+    "item 4",
+  ];
+
+ late List<bool> items;
+   
+    @override
+   void initState() {
+     super.initState(
+     );
+     items= List.generate(choice.length, (index) => false);
+     
+   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +36,30 @@ class _CheckListState extends State<CheckList> {
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ),
-        actions:  [
-          IconButton(onPressed: () {
-            
-          }, icon: const Icon(Icons.playlist_add_check_circle_rounded))
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.playlist_add_check_circle_rounded))
         ],
       ),
+      body: ListView.builder(itemBuilder: 
+      (context, index) {
+        return ListTile(
+          title: Text(choice[index]),
+          tileColor: items[index]?Colors.grey:null,
+          trailing: items[index]?const Icon(Bootstrap.check):null,
+          onTap: () {
+            _toggleremover(index);
+          },
+        );
+      },
+      itemCount: choice.length),
     );
+  }
+
+  void _toggleremover(int index) {
+    setState(() {
+     items[index]=!items[index];
+    });
   }
 }
