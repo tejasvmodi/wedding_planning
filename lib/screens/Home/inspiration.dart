5cb28f30a0b4bde30f0cadfda525119305ebfbc4
tejasvmodi <moditejasv@gmail.com>
2023-11-26 21:02:16 +0530

    commit at 21:02 26-11-2023

--- lib/screens/Home/inspiration.dart
+++ lib/screens/Home/inspiration.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:wedding_application/screens/components/category_bar.dart';
 import 'package:wedding_application/screens/components/custom_inspiration_app_bar.dart';
+import 'package:wedding_application/screens/components/explore_tile.dart';
 import 'package:wedding_application/screens/components/persistance_header.dart';
 
 class Inspiration extends StatelessWidget {
@@ -23,7 +24,25 @@ class Inspiration extends StatelessWidget {
               ),
             ),
           ),
-          
+          SliverStaggeredGrid.countBuilder(
+            mainAxisSpacing: 1,
+            crossAxisSpacing: 1,
+              crossAxisCount: 3,
+              staggeredTileBuilder: (int index) {
+                int modedIndex=index %20;
+                int cXCellCount = modedIndex == 11 ?2:1;
+
+                double mXCellCount=1;
+
+                if(modedIndex ==2 || modedIndex==2){
+                  mXCellCount=2;
+                }
+                return StaggeredTile.count(cXCellCount, mXCellCount);
+              },
+              itemBuilder: (BuildContext  context, int  index) {
+                return ExploreTil();
+              },
+              itemCount: 20)
         ],
       )),
     );
---
+++ lib/screens/components/explore_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ExploreTil extends StatelessWidget {
+  const ExploreTil({super.key});
+  // final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink,
+    );
+  }
+}
